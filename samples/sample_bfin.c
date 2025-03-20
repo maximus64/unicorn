c13c6001584034255b4ceea8afcb326fbf557fc5
Khoa Hoang <admin@khoahoang.com>
2025-03-19 23:06:37 -0700

    sample_bfin.c: add hook for unmap memory write

--- samples/sample_bfin.c
+++ samples/sample_bfin.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 // code to be emulated
-// r0.l = 0x4141; r0.h = 0x4242; r1 = 0x32
+// TODO: FIX ME: r0.l = 0x4141; r0.h = 0x4242; r1 = 0x32
 #define CODE "\x00\xe1\x41\x41\x03\x20\x00\xe1\xd0\xba\x40\xe1\x42\x42"
 
 // memory address where emulation starts
@@ -28,11 +28,32 @@ static void hook_code(uc_engine *uc, uint64_t address, uint32_t size,
            address, size);
 }
 
+// callback for tracing memory access (READ or WRITE)
+static bool hook_mem_invalid(uc_engine *uc, uc_mem_type type, uint64_t address,
+                             int size, int64_t value, void *user_data)
+{
+    switch (type) {
+    default:
+        // return false to indicate we want to stop emulation
+        return false;
+    case UC_MEM_READ_UNMAPPED:
+        printf(">>> Missing memory is being READ at 0x%" PRIx64
+               ", data size = %u\n",
+               address, size);
+        return false;
+    case UC_MEM_WRITE_UNMAPPED:
+        printf(">>> Missing memory is being WRITE at 0x%" PRIx64
+               ", data size = %u, data value = 0x%" PRIx64 "\n",
+               address, size, value);
+        return false;
+    }
+}
+
 static void test_blackfin(void)
 {
     uc_engine *uc;
     uc_err err;
-    uc_hook trace1, trace2;
+    uc_hook trace1, trace2, trace3;
 
     uint32_t r0 = 0x0; // r0 register
     uint32_t r1 = 0x0; // r1 register
@@ -60,11 +81,17 @@ static void test_blackfin(void)
     uc_hook_add(uc, &trace2, UC_HOOK_CODE, hook_code, NULL, ADDRESS,
                 ADDRESS + sizeof(CODE) - 1);
 
+    // intercept invalid memory events
+    uc_hook_add(uc, &trace3,
+                UC_HOOK_MEM_READ_UNMAPPED | UC_HOOK_MEM_WRITE_UNMAPPED,
+                hook_mem_invalid, NULL, 1, 0);
+
     // emulate machine code in infinite time (last param = 0), or when
     // finishing all the code.
     err = uc_emu_start(uc, ADDRESS, ADDRESS + sizeof(CODE) - 1, 0, 0);
     if (err) {
-        printf("Failed on uc_emu_start() with error returned: %u\n", err);
+        printf("Failed on uc_emu_start() with error returned: %u (%s)\n",
+               err, uc_strerror(err));
     }
 
     // now print out some registers
