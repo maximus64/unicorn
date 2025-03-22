a9cec98f70683db24283609d0b38d270cb2aefc8
Khoa Hoang <admin@khoahoang.com>
2025-03-21 19:26:26 -0700

    test_bfin.c: add relative jump test code

--- tests/unit/test_bfin.c
+++ tests/unit/test_bfin.c
@@ -31,4 +31,36 @@ static void test_bfin_mov(void)
     OK(uc_close(uc));
 }
 
-TEST_LIST = {{"test_bfin_mov", test_bfin_mov}, {NULL, NULL}};
+static void test_bfin_relative_jump(void)
+{
+    /*
+     * test:
+     *     r0.l = 0x4141
+     *     jump 1f
+     *     r0.l = 0xbad0
+     * 1:
+     *     r0.h = 0x4242
+     */
+    char code[] = "\x00\xe1\x41\x41\x03\x20\x00\xe1\xd0\xba\x40\xe1\x42\x42"; // r0.l = 0x4141
+    uint32_t r_pc, r_r0;
+    uc_engine *uc;
+
+    uc_common_setup(&uc, UC_ARCH_BFIN, UC_MODE_LITTLE_ENDIAN, code,
+                    sizeof(code) - 1);
+
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code) - 1, 0, 0));
+
+    OK(uc_reg_read(uc, UC_BFIN_REG_R0, &r_r0));
+    OK(uc_reg_read(uc, UC_BFIN_REG_PC, &r_pc));
+
+    TEST_CHECK(r_r0 == 0x42424141);
+    TEST_CHECK(r_pc == code_start + sizeof(code) - 1);
+
+    OK(uc_close(uc));
+}
+
+TEST_LIST = {
+                {"test_bfin_mov", test_bfin_mov},
+                {"test_bfin_relative_jump", test_bfin_relative_jump},
+                {NULL, NULL}
+            };
